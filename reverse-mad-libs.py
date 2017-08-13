005115b8cd57f69fff3eb430690d31e6fd7873d4
Rahul Kathuria <rahulkathuria52@gmail.com>
2017-08-12 20:00:00 -0600

    Added raw questions

---
+++ reverse-mad-libs.py
@@ -0,0 +1,25 @@
+
+ level_1 = 
+ '''Narendra Damodardas __1__, born 17 September 1950 is an Indian politician who is the 
+ 14th and current Prime Minister of __2__, in office since May 2014. He was the Chief Minister of __3__ 
+ from 2001 to 2014, and is the Member of __4__ for Varanasi. Modi, a member of the Bharatiya Janata
+ __5__ (BJP),  is a Hindu nationalist and member of the right-wing Rashtriya Swayamsevak Sangh (RSS).'''
+
+ level_1_answers=["Modi","India","Gujarat","Parliament","Party"]
+
+ level_2=
+ '''Android is a mobile operating system developed by __1__, based on the __2__ kernel and designed 
+ primarily for touchscreen mobile devices such as smartphones and tablets. Android's user __3__ is 
+ mainly based on direct manipulation, using __4__ gestures that loosely correspond to real-world actions, 
+ such as swiping, tapping and pinching, to manipulate on-screen objects, along with a virtual __5__
+ or text input.'''
+
+ level_2_answers=["Google","Linux","interface","touch","keyboard"]
+
+ level_3=
+'''Facebook is an American for-profit corporation and an online __1__ media
+ and __1__ networking service based in Menlo Park, California. The Facebook website was launched 
+ on February 4, 2004, by __2__ Zuckerberg, along with fellow __3__ College students and roommates,
+ Eduardo Saverin, Andrew McCollum, __4__ Moskovitz, and __5__ Hughes.'''
+
+ level_3_answers=["social","Mark","Harvard","Dustin","Chris"]
